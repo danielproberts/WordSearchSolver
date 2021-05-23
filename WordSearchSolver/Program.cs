0b77ab760bb4a7d85771ee1384b649778c302b5c
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-05-23 12:43:11 -0400

    basic match working

--- WordSearchSolver/Program.cs
+++ WordSearchSolver/Program.cs
@@ -7,15 +7,21 @@ namespace WordSearchSolver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FindWord("test", "teaktest");
+            Console.WriteLine(FindWord("test", "teaktestjadtestfakj"));
+            Console.ReadKey();
         }
 
         public static int FindWord(string s, string m)
         {
+            /*
             char[] splitWord = s.ToCharArray();
             char[] splitMatch = m.ToCharArray();
-            Regex rx = new Regex(@"(${matchWord})", RegexOptions.IgnoreCase);
-            MatchCollection matches = rx.Matches(s);
+            */
+            
+            Regex rx = new Regex(@"(\W*((?i))" + s + @"((?-i))\W*)", RegexOptions.IgnoreCase);
+
+            //Regex rx = new Regex(@"(test)", RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(m);
             return matches.Count;
         }
     }
