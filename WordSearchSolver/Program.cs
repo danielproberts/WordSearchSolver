a639af27d43594a00c2bfcf8ff600f9318a554ec
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-05-23 12:09:48 -0400

    Add project files.

---
+++ WordSearchSolver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace WordSearchSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindWord("test", "teaktest");
+        }
+
+        public static int FindWord(string s, string m)
+        {
+            char[] splitWord = s.ToCharArray();
+            char[] splitMatch = m.ToCharArray();
+            Regex rx = new Regex(@"(${matchWord})", RegexOptions.IgnoreCase);
+            MatchCollection matches = rx.Matches(s);
+            return matches.Count;
+        }
+    }
+}
